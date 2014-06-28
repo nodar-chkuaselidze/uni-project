cab119cdbd57e8405c1b1419fb966be16b21ef94
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-29 01:11:58 +0400

    Move system bootstrap code to one file

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 'use strict';
 
-global.rapp = require('./app/helpers/require.js')('app', true);
+require('./configs/bootstrap.js')(__dirname);
 
-var nconf = require('nconf').argv().env(),
-    env   = nconf.get('NODE_ENV'),
-    dbg   = require('debug')('app');
-
-//keep globals to minimum
-global.ROOT     = __dirname;
-global.APP_ROOT = __dirname + '/app';
-global.ENV      = env ? env : 'production';
-
-nconf.file(ROOT + '/configs/' + (env === 'production' ? 'config' : 'local') + '.json');
-
-var index = rapp('index'),
-    app   = rapp('router');
+var app = rapp('router'),
+    nconf = require('nconf');
 
 
 app.listen(nconf.get('port'), function () {
---
+++ configs/bootstrap.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports = module.exports = function (__dirname) {
+  //keep globals to minimum
+  global.ROOT     = __dirname;
+  global.APP_ROOT = __dirname + '/app';
+  global.rapp     = require(APP_ROOT + '/helpers/require.js')(APP_ROOT, true);
+
+  var nconf = require('nconf').argv().env(),
+      env   = nconf.get('NODE_ENV'),
+      dbg   = require('debug')('app');
+
+  global.ENV = env ? env : 'production';
+
+  nconf.file(ROOT + '/configs/' + (env === 'production' ? 'config' : 'local') + '.json');
+};
