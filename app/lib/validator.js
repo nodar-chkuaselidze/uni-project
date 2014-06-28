eda8c07c1c424a02a7f46f5071ecbef25fa2dbfe
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-28 19:31:19 +0400

    Add geoAlpha validator

--- app/lib/validator.js
+++ app/lib/validator.js
@@ -3,4 +3,8 @@
 //for custom validators
 var validator = require('validator');
 
+validator.extend('geoAlpha', function (str) {
+  return /^[ა-ჰ]+$/.test(str);
+});
+
 exports = module.exports = validator;
