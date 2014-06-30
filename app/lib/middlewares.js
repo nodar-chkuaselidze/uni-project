fb4f9059061804cb113b1aa73a6b5c9df286ab25
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 23:42:19 +0400

    Change admin to lecturer and add admin too

--- app/lib/middlewares.js
+++ app/lib/middlewares.js
@@ -3,11 +3,13 @@
 var User = rapp('models/user');
 
 exports.user  = {
-};
-
-exports.admin = {
   isAdmin : function (req, res, next) {
     req.isAdmin = req.isAuthenticated() && req.user.hasRole('admin');
+
+    next();
+  },
+  isLecturer : function (req, res, next) {
+    req.isLecturer = req.isAuthenticated() && req.user.hasRole('lecturer');
 
     next();
   }
