cb578e8853d57ab9a2a59feba70bab6b67be9223
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 16:09:43 +0400

    Add passport to express

--- app/lib/express.js
+++ app/lib/express.js
@@ -6,6 +6,7 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   path = require('path'),
   expressValidator = require('express-validator'),
+  passport = rapp('lib/passport'),
   app  = express();
 
 app.set('view engine', 'jade');
@@ -15,6 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(expressValidator());
 app.use(cookieParser());
+app.use(expressSession({ secret : 'Some dummy text, still has to be in configs'}));
 app.use(express.static(ROOT + '/public'));
+
+app.use(passport.initialize());
 
 module.exports = { app : app, express : express };
