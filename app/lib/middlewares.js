2eeea6254ad6dd9a893b993e54f45e8ea114dde6
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 16:10:04 +0400

    Add admin.isAdmin middleware

--- app/lib/middlewares.js
+++ app/lib/middlewares.js
@@ -1,3 +1,14 @@
 'use strict';
 
 var User = rapp('models/user');
+
+exports.user  = {
+};
+
+exports.admin = {
+  isAdmin : function (req, res, next) {
+    req.isAdmin = req.isAuthenticated() && req.user.hasRole('admin');
+
+    next();
+  }
+};
