613ab656cb5cbad57adfed2cff1dbf9722501d9c
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 23:51:55 +0400

    Add Lecturer/Admin checker

--- app/lib/middlewares.js
+++ app/lib/middlewares.js
@@ -3,6 +3,21 @@
 var User = rapp('models/user');
 
 exports.user  = {
+  reqAdmin : function (req, res, next) {
+    if (req.isAuthenticated() && !req.user.hasRole('admin')) {
+      req.flash('error', 'თქვენ არ გაქვთ ადმინისტრატორის უფლებები');
+      res.redirect('/lecturer/');
+    }
+  },
+
+  checkLecturer : function (req, res, next) {
+    if (req.isAuthenticated() && !req.user.hasRole('lecturer')) {
+      req.flash('error', 'თქვენ არ გაქვთ ლექტორის უფლებები');
+      res.redirect('/');
+    } else {
+      next();
+    }
+  }
   isAdmin : function (req, res, next) {
     req.isAdmin = req.isAuthenticated() && req.user.hasRole('admin');
 
