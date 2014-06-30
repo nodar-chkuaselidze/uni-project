516d707b791087346a80ac4f4ae9a16a2188cf01
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 13:41:48 +0400

    Add admin routes file to router

--- app/router.js
+++ app/router.js
@@ -3,6 +3,7 @@
 var app = rapp('lib/express').app,
   router = rapp('routes');
 
-app.use(rapp('routes'));
+app.use('/',      rapp('routes/index'));
+app.use('/admin', rapp('routes/admin'));
 
 module.exports = app;
