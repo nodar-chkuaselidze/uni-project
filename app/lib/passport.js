6ef7f46bf3be022d6d036b9b88b258a558e3dd30
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-06-30 23:42:56 +0400

    implement local auth to passport

--- app/lib/passport.js
+++ app/lib/passport.js
@@ -1,16 +1,55 @@
 'use strict';
 
 var passport = require('passport'),
-    LocalStategy = require('passport-local').Strategy;
+    LocalStategy = require('passport-local').Strategy,
+    bcrypt = require('bcryptjs'),
+    debug = require('debug')('lib:passport'),
+    User  = rapp('models/user');
 
 passport.use(new LocalStategy({
   passReqToCallback : true,
   usernameField : 'email',
   passwordField : 'password'
-}, function (req, username, password, verify) {
-  console.log(username + ' . ' + password);
+}, function (req, email, password, next) {
+  User.findOneQ({
+    email     : req.body.email,
+    firstName : req.body.firstname,
+    lastName  : req.body.lastname
+  }, {
+    email : true,
+    firstName : true,
+    lastName : true,
+    roles : true,
+    hashedPassword : true
+  }).then(function (user) {
+    var message = 'ინფორმაცია არასწორია';
 
-  verify('Error found');
+    if (user && bcrypt.compareSync(password, user.hashedPassword)) {
+      next(null, user);
+    } else {
+      next(null, false, { message : message });
+    }
+  }).fail(function (error) {
+    if (error) {
+      debug(error);
+    }
+
+    next('Server Error');
+  });
 }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findOneQ({ _id : id })
+    .then(function (user) {
+      done(null, user);
+    })
+    .fail(function (error) {
+      done(error);
+    });
+});
 
 exports = module.exports = passport;
