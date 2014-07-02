9989886c7f75b12517ceb5286882af2b650b0c52
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-02 14:55:00 +0400

    Handle route to controller

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,10 +3,18 @@
 var express = rapp('lib/express').express,
   router = express.Router(),
   apiMiddlewares = rapp('routes/middlewares/api'),
+  apiControllers = rapp('controllers/api'),
   passport = rapp('lib/passport');
 
 router.post('/change-password', apiMiddlewares.ensureAuth, function (req, res) {
-  res.json(404, {});
+  apiControllers.changePassword(req).then(function () {
+    console.log('password should have changed');
+    res.json(404, {});
+  })
+  .catch(function (error) {
+    console.log('password should not have changed, we got errors');
+    res.json(error.status, error.list);
+  });
 });
 
 
