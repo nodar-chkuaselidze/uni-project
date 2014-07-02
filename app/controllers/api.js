aa43956a856211a5da1744da88bab7521246fdfc
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-02 17:57:57 +0400

    Validate Password and check if it fails, handle fails

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,17 +1,37 @@
 'use strict';
 
 var apiControllers = {},
+    debug = require('debug')('app:controllers:api'),
+    _ = require('lodash'),
     Q = require('q');
-
 
 apiControllers.changePassword = function (req) {
   req.checkBody('oldPassword', 'ძველი და ახალი პაროლი ერთია').notEquals(req.body.newPassword);
   req.checkBody('newPassword', 'პაროლი უნდა შედგებოდეს მინიმუმ 6 სიმბოლოთი').isLength(6);
   req.checkBody('newPassword', 'განმეორებითი პაროლი არ დაემთხვა').equals(req.body.repeatNewPassword);
 
-  //var errors = req.validationErrors();
-  
-  return req.validationErrorsQ();
+  debug('make password validations');
+  return req.validationErrorsQ().then(function () {
+      debug('Check if password is okay');
+
+      return Q(req.user.isPasswordQ(req.body.oldPassword))
+    }).then(function () {
+      console.log('somewhere here');
+    }).catch(function (errors) {
+      debug('We found error');
+
+      switch (true) {
+        case errors instanceof Error:
+          errors = errors.message;
+        case typeof errors === "string":
+          errors = [ errors ];
+      }
+
+      return Q.reject({
+        status : 400,
+        list   : errors
+      });
+    });
 };
 
 exports = module.exports = apiControllers;
