19cc03a66e59b5e53f68894ac3d9b6667a56e907
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-02 14:54:41 +0400

    Use validator from express-validator

--- app/lib/validator.js
+++ app/lib/validator.js
@@ -1,10 +1,14 @@
 'use strict';
 
 //for custom validators
-var validator = require('validator');
+var validator = rapp('lib/express-validator').validator;
 
 validator.extend('geoAlpha', function (str) {
   return /^[ა-ჰ]+$/.test(str);
+});
+
+validator.extend('notEquals', function (str, str2) {
+  return !validator.equals(str, str2);
 });
 
 exports = module.exports = validator;
