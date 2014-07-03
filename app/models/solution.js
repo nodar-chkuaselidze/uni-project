4d4537677ccd957ae2b7004fcfc928d9a527e717
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-03 14:24:23 +0400

    Init models

---
+++ app/models/solution.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var
+  validator = rapp('lib/validator'),
+  mongoose = rapp('lib/mongoose'),
+  nconf = require('nconf'),
+  debug = require('debug')('app:models:test'),
+  Q = require('q'),
+
+SolutionSchema = new mongoose.Schema({
+  ID : {
+    unique   : true,
+    type     : String,
+    required : true
+  },
+  firstName : {
+    type     : String,
+    required : true,
+    validate : [validator.geoAlpha, 'გამოიყენეთ მხოლოდ ქართული სიმბოლოები']
+  },
+  lastName : {
+    type     : String,
+    required : true,
+    validate : [validator.geoAlpha, 'გამოიყენეთ მხოლოდ ქართული სიმბოლოები']
+  },
+  answers : {
+    type     : Array,
+    required : true
+  },
+  score : {
+    type     : Number,
+    required : true,
+    default  : 0
+  }
+});
+
+exports = module.exports = mongoose.model('Solution', SolutionSchema);
---
+++ app/models/test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var
+  validator = rapp('lib/validator'),
+  mongoose = rapp('lib/mongoose'),
+  nconf = require('nconf'),
+  debug = require('debug')('app:models:test'),
+  Q = require('q'),
+
+TestSchema = new mongoose.Schema({
+  owner : {
+    unique   : true,
+    type     : String,
+    required : true,
+    validate : [validator.isEmail, 'e-mail არასწორია']
+  },
+  subject : {
+    type : String,
+    required : true
+  },
+  questions : {
+    type : Array,
+    required : true,
+  }
+});
+
+exports = module.exports = mongoose.model('Test', TestSchema);
