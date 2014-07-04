cde9f7cb264d1ad73718814bbb7e7a6412a02300
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-04 23:02:23 +0400

    Add route for tests

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,28 @@
 'use strict';
 
 var express = rapp('lib/express').express,
+  userController = rapp('controllers/user'),
   router = express.Router();
 
 router.get('/', function (req, res) {
   res.render('index', {
     pageTitle : 'Nothing interesting'
+  });
+});
+
+router.get('/test/:test_id/', function (req, res) {
+  userController.getTestById(req)
+  .then(function (test) {
+    console.log(test);
+    res.render('test', {
+      test : test
+    });
+  })
+  .catch(function (error) {
+    res.status(error.status);
+    res.render('test', {
+      message : error
+    });
   });
 });
 
