003088f1b2ac1787e50164fcd3694e56bcb919fc
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-04 17:51:33 +0400

    Add checkLecture method for API's

--- app/routes/middlewares/api.js
+++ app/routes/middlewares/api.js
@@ -4,7 +4,17 @@ exports = module.exports = {
   ensureAuth : function (req, res, next) {
     if (!req.isAuthenticated() || !req.user) {
       res.json(403, {
-        errors : [ 'თქვენ არ ხართ სისტემაში' ]
+        errors : [ 'თქვენ არ გაქვთ სათანადო უფლება' ]
+      });
+      return;
+    }
+
+    next();
+  },
+  checkLecturer : function (req, res, next) {
+    if (!req.isAuthenticated() || !req.user || !req.user.hasRole('lecturer')) {
+      res.json(403, {
+        errors : [ 'თქვენ არ გაქვთ სათანადო უფლება' ]
       });
       return;
     }
