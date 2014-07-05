0fd597b07c212a17c21f377c91c259f825c326aa
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-05 17:15:10 +0400

    Add notEmpty validator for mongoose

--- app/lib/validator.js
+++ app/lib/validator.js
@@ -7,6 +7,10 @@ validator.extend('geoAlpha', function (str) {
   return /^[ა-ჰ]+$/.test(str);
 });
 
+validator.extend('notEmpty', function (str) {
+  return str && str.length > 0;
+});
+
 validator.extend('notEquals', function (str, str2) {
   return !validator.equals(str, str2);
 });
