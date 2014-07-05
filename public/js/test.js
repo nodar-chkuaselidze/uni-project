fb746dfeb5b15afa99755eafa45d584a744a69aa
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-05 15:02:13 +0400

    Handle errors from API

--- public/js/test.js
+++ public/js/test.js
@@ -1,7 +1,31 @@
 $("#testForm").submit(function () {
-  var data = $(this).serialize();
+  var form = $(this),
+    data = form.serialize(),
+    alert = form.find('.alert');
 
-  console.log(data);
+  $.ajax({
+    url : '/api/v1/send-test',
+    method : 'POST',
+    dataType : 'json',
+    data : data
+  }).success(function (data) {
+    console.log(data);
+  }).error(function (ajax) {
+    var errors = ajax.responseJSON;
+
+    if (!errors || !errors instanceof Array) {
+      errors = [ 'ინფორმაციის შენახვა არ მოხერხდა' ];
+    }
+
+    alert.removeClass('hide').addClass('alert-danger');
+    var list = $('<ul></ul>');
+
+    errors.forEach(function (error) {
+      list.append('<li>' + error + '</li>');
+    });
+
+    alert.find('.alert-text').html(list);
+  });
 
   return false;
 });
