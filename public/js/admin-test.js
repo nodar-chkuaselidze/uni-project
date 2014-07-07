bd8343294b7a4b1f61ee4e3527a89d6bb1257261
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-07-07 20:12:29 +0400

    Make angular model as mongo object

--- public/js/admin-test.js
+++ public/js/admin-test.js
@@ -1,24 +1,21 @@
 var addTestApp = angular.module('addTestApp', []);
 
 addTestApp.controller('addTestCtrl', [ '$scope', '$http', function ($scope, $http) {
-  var test = {
-    subject  : '',
-    maxScore : 0
+
+  $scope.test = {
+    subject   : '',
+    maxScore  : 0,
+    questions : []
   };
 
-  $scope.test = test;
-  $scope.list = [];
-
-  test.list = $scope.list;
-
-  $scope.$watch('list', function (newVal) {
-    test.maxScore = $scope.list.reduce(function (prev, curr) {
+  $scope.$watch('test.questions', function (newVal) {
+    $scope.test.maxScore = $scope.test.questions.reduce(function (prev, curr) {
       return prev + +curr.rightAnswer;
     }, 0);
   }, true);
 
   $scope.addCase = function () {
-    $scope.list.push({
+    $scope.test.questions.push({
       question : '',
       answers  : [ { correct : false,  text : '' } ],
       rightAnswer : 1,
@@ -27,19 +24,16 @@ addTestApp.controller('addTestCtrl', [ '$scope', '$http', function ($scope, $htt
   };
 
   $scope.removeCase   = function (listItemIndex) {
-    $scope.list.splice(listItemIndex, 1);
+    $scope.test.questions.splice(listItemIndex, 1);
   };
 
   $scope.removeAnswer = function (listItemIndex, answerIndex) {
-    $scope.list[listItemIndex].answers.splice(answerIndex, 1);
+    $scope.test.questions[listItemIndex].answers.splice(answerIndex, 1);
   };
 
   $scope.saveTest = function () {
-    console.log(test);
-
-    $http.post('/api/v1/add-test', test)
+    $http.post('/api/v1/add-test', $scope.test)
       .success(function (data, status) {
-        console.log(data, status);
         window.location.reload();
       })
       .error(function (data, status) {
